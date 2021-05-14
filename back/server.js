fcf8978334a2e72537ddc1b359ed173506f27f20
marianosolberg <marianosolberg@hotmail.com>
2021-05-14 12:16:10 -0300

    modificaciones

--- back/server.js
+++ back/server.js
@@ -1,4 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 
-app.use("/api", routes);
+// app.use("/api", routes);
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`server listening on ${PORT}`);
+});
