8b2fb5b5d6d14628f284097bd11e13ef1d2e4d4f
marianosolberg <marianosolberg@hotmail.com>
2021-06-07 15:03:22 -0300

    cambio

--- back/config/db.js
+++ back/config/db.js
@@ -2,14 +2,10 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://user2021:user2021@basedatos.ashm7.mongodb.net/muestra?retryWrites=true&w=majority" ||
-        process.env.MONGO_ADDRESS,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    await mongoose.connect(process.env.MONGO_ADDRESS, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("mongoose connection Success");
   } catch (error) {
     console.log(error);
